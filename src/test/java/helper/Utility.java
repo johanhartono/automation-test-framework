3e9147e9093aeb9db75912757ab9e83e67e574be
johanhartono <johanhartono@gmail.com>
2024-08-24 20:39:40 +0700

    update

--- src/test/java/helper/Utility.java
+++ src/test/java/helper/Utility.java
@@ -31,7 +31,7 @@ public class Utility {
 
     public static void startDriver() {
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-allow-origins=*");
